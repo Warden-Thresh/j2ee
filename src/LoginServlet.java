50ad9544079cc203461e000fb33a1a7c3019093e
YangJiaYing <1570263943@qq.com>
2017-09-06 18:59:21 +0800

    get the post parameter

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.HTML;
 
 public class LoginServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
@@ -11,7 +13,14 @@ public class LoginServlet extends HttpServlet{
 		String password = request.getParameter("password");
 		System.out.println("name:"+name);
 		System.out.println("password:"+password);
-		response.getWriter().println("name£º"+name+"\n"+"password£º"+password);
+		String html =null;
+		if ("admin".equals(name)&&"123".equals(password)) {
+			html = "<div style ='color:green'>success</div>";
+		}else{
+			html = "<div style='color:red'>fail</div>";
+		}
+		PrintWriter printWriter =  response.getWriter();
+		printWriter.println(html);
 	}
 	
 }
